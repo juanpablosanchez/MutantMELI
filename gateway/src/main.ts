6557ac7651b1ea38a2a8095e1f681ecda62f026d
JuanPablo <jupsanchezri@unal.edu.co>
2022-07-11 15:48:09 -0500

    yml, generar artifact de dist

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 80;
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
